eb4f62a08b592ccedf0af68eee52ecaec2205ccf
João Lucas <joaolucascp16@gmail.com>
2023-05-28 14:08:30 -0300

    create(Container): creates a container component to wrap the html elements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,9 @@
+import { Container } from '@/components/Container'
+
 const Home = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Container>Hello World!</Container>
     </div>
   )
 }
