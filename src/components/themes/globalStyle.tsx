a1d8b592c7fc609056af310eecfb268f58520831
João Lucas <joaolucascp16@gmail.com>
2023-05-29 10:17:43 -0300

    add(globalStyle): add K2D font-family

--- src/components/themes/globalStyle.tsx
+++ src/components/themes/globalStyle.tsx
@@ -1,6 +1,7 @@
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=K2D:wght@200;400;500;600;700&display=swap');
   * {
     margin: 0;
     padding: 0;
