eb3eb25705783295633f7317ca2ee8ffe5aaf2b1
João Lucas <joaolucascp16@gmail.com>
2023-05-31 19:12:54 -0300

    Update index.tsx

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,11 +1,10 @@
-import * as S from './styles'
 import { SpaProps } from '@/types/spa'
 
 const Admin = (props: SpaProps) => {
   return (
-    <S.Wrapper>
+    <div>
       <></>
-    </S.Wrapper>
+    </div>
   )
 }
 
