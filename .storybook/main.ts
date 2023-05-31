545a45bb7c82654c4eb3e65716c7cab1db73f0c8
João Lucas <joaolucascp16@gmail.com>
2023-05-31 19:09:50 -0300

    update(storybook.main.ts): webpackFinal, add the '@' alias

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,8 @@
-import type { StorybookConfig } from '@storybook/nextjs'
+import type { StorybookConfig } from '@storybook/nextjs';
+import path from 'path';
+
+const toPath = (_path) => path.join(process.cwd(), _path);
+
 const config: StorybookConfig = {
   stories: ['../src/components/**/stories.tsx'],
   addons: [
@@ -13,6 +17,19 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag'
+  },
+  webpackFinal: async (storybookWebpackConfig, { configType }) => {
+    const { resolve = {} } = storybookWebpackConfig;
+    const { alias = {} } = resolve;
+    // Add the '@' alias
+    alias['@'] = toPath('src');
+
+    // Update the config
+    storybookWebpackConfig.resolve = { ...resolve, alias };
+
+    // Return the updated config
+    return storybookWebpackConfig;
   }
-}
-export default config
+};
+
+export default config;
