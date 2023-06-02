daee63eb02b983adfa9c31052fadfd9c4d8b97a1
João Lucas <joaolucascp16@gmail.com>
2023-06-02 00:58:02 -0300

    create button types

---
+++ src/types/button.ts
@@ -0,0 +1,14 @@
+import { ReactElement } from 'react'
+import { Colors } from '@/types/colors'
+import { FontSizeTypes, FontWeightTypes } from '@/types/font'
+
+export type ButtonProps = {
+  text: string
+  icon?: ReactElement
+  iconPosition?: 'left' | 'right'
+  bgColor?: Colors
+  color?: Colors
+  fontSize?: FontSizeTypes
+  fontWeight?: FontWeightTypes
+  onClick: () => void
+}
