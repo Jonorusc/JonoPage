f0545f4207cb1e3cf129b8ab07dfee7f786b6ebb
João Lucas <joaolucascp16@gmail.com>
2023-06-06 14:24:16 -0300

    create(types/input): create types for input component

---
+++ src/types/input.ts
@@ -0,0 +1,18 @@
+import React from 'react'
+import { FontSizeTypes } from './font'
+
+export type EventType = React.ChangeEvent<
+  HTMLInputElement | HTMLTextAreaElement
+>
+type InputValue = string | number
+
+export type InputProps<T extends (event?: EventType) => void | null> = {
+  hasError?: boolean
+  isDisabled?: boolean
+  placeholder?: string
+  value?: InputValue
+  type: 'text' | 'email' | 'password' | 'textarea'
+  onInputChange: T
+  name: string
+  fontSize: FontSizeTypes
+}
