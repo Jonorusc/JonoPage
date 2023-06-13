892d893f7b8f82a471efd717244bf58f1d583aa4
João Lucas <joaolucascp16@gmail.com>
2023-06-13 17:33:09 -0300

    update(types/form.ts)

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,6 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { ReactElement, ReactNode, JSXElementConstructor } from 'react'
 
+export type InputValue = { [key: string]: string }
+
 export type ChildrenProps =
   | ReactElement<any, string | JSXElementConstructor<any>>
   | React.ReactElement<any, string | JSXElementConstructor<any>>[]
@@ -12,17 +14,16 @@ export type ChildrenProps =
   | number
   | boolean
 
+export type FormCallBackFunction = (
+  e?: React.FormEvent<HTMLFormElement>,
+  formValues?: InputValue
+) => void
+
 export type FormProps = {
-  onSubmit: (
-    e?: React.FormEvent<HTMLFormElement>,
-    formValues?: { [key: string]: string }
-  ) => void
-  onReset?: (
-    e?: React.FormEvent<HTMLFormElement>,
-    formValues?: { [key: string]: string }
-  ) => void
+  onSubmit: FormCallBackFunction
+  onReset?: FormCallBackFunction
   children: ChildrenProps
   resetName?: string
   submitName?: string
-  inputValues?: { [key: string]: string }
+  inputValues?: InputValue
 }
