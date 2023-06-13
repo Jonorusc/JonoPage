6a2ff3bde0cd4e8c4cd61bf8fc86b1dd7d308e21
João Lucas <joaolucascp16@gmail.com>
2023-06-13 12:51:09 -0300

    update(components/button/stories.tsx)

--- src/components/Button/stories.tsx
+++ src/components/Button/stories.tsx
@@ -1,4 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 import Button from '.'
 
 import { FaInstagram } from 'react-icons/fa'
@@ -8,12 +9,13 @@ const meta: Meta<typeof Button> = {
   component: Button,
   args: {
     text: 'Button',
-    onClick: () => console.log('clicked'),
+    onClick: (event) => action('clicked')(event),
     bgColor: 'darker',
     color: 'primary',
     fontSize: 'medium',
     fontWeight: 'normal',
-    icon: <FaInstagram />
+    icon: <FaInstagram />,
+    type: 'button'
   },
   argTypes: {
     fontWeight: {
@@ -26,6 +28,12 @@ const meta: Meta<typeof Button> = {
       control: {
         type: 'select',
         options: ['small', 'medium', 'large', 'xlarge']
+      }
+    },
+    type: {
+      control: {
+        type: 'select',
+        options: ['button', 'submit', 'reset']
       }
     }
   }
