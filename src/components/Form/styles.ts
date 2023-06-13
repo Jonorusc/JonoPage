d19bc8ba96e73101b3090f3d4debf4849783d225
João Lucas <joaolucascp16@gmail.com>
2023-06-13 12:52:38 -0300

    create(components/form/styles.ts): styled components

---
+++ src/components/Form/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Form = styled.form``
+export const FormButtons = styled.div`
+  display: flex;
+  column-gap: 1rem;
+  margin-top: 2rem;
+`
+// export const Button = styled.button``
