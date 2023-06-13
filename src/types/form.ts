c51ceb1aca4f73d09fb074702877fde348acd3b5
João Lucas <joaolucascp16@gmail.com>
2023-06-13 12:41:06 -0300

    Create form.ts

---
+++ src/types/form.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ReactElement, ReactNode, JSXElementConstructor } from 'react'
+
+export type ChildrenProps =
+  | ReactElement<any, string | JSXElementConstructor<any>>
+  | React.ReactElement<any, string | JSXElementConstructor<any>>[]
+  | ReactNode
+  | ReactNode[]
+  | null
+  | undefined
+  | string
+  | number
+  | boolean
+
+export type FormProps = {
+  onSubmit: (
+    e?: React.FormEvent<HTMLFormElement>,
+    formValues?: { [key: string]: string }
+  ) => void
+  onReset?: (
+    e?: React.FormEvent<HTMLFormElement>,
+    formValues?: { [key: string]: string }
+  ) => void
+  children: ChildrenProps
+  resetName?: string
+  submitName?: string
+}
