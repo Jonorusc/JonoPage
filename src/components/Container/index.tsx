ea6632a5ab0309991556fcb3cf412d07d544dbda
João Lucas <joaolucascp16@gmail.com>
2023-06-26 19:01:27 -0300

    Update index.tsx

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -1,10 +1,13 @@
 import styled, { css } from 'styled-components'
 
-export const Container = styled.div`
-  ${({ theme }) => css`
+export const Container = styled.div<{ padding?: string }>`
+  ${({ theme, padding }) => css`
     width: 100%;
     max-width: ${theme.containerMaxWidth};
     margin-left: auto;
     margin-right: auto;
+    padding-left: ${theme.spacing.large};
+    padding-right: ${theme.spacing.large};
+    ${!!padding && `padding: ${padding};`}
   `}
 `
