03b71e095e87f83ab1c4b0b8aa09a017f561d89c
João Lucas <joaolucascp16@gmail.com>
2023-06-28 13:48:38 -0300

    Update withAuth.ts

--- src/utils/withAuth.ts
+++ src/utils/withAuth.ts
@@ -1,7 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import { getCurrentUser } from '@/firebase/auth'
 import { useRouter } from 'next/router'
+import { User } from 'firebase/auth'
 type Props = {
   children:
     | React.ReactComponentElement<any>
@@ -11,13 +12,21 @@ type Props = {
 }
 
 const WithAuth = ({ children }: Props) => {
-  const router = useRouter()
-  const user = getCurrentUser()
+  const [user, setUser] = useState<User | null>(null)
 
+  const router = useRouter()
   useEffect(() => {
-    if (!user) {
-      router.push('/admin/login')
+    const getUser = async () => {
+      await getCurrentUser().then((res) => {
+        if (res) {
+          setUser(res)
+        } else {
+          router.push('/admin/login')
+        }
+      })
     }
+
+    getUser()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
