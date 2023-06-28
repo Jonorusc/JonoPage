8e93abdeb20f661816af681aca1d01020e91f50c
João Lucas <joaolucascp16@gmail.com>
2023-06-28 11:43:58 -0300

    Update index.ts

--- src/firebase/auth/index.ts
+++ src/firebase/auth/index.ts
@@ -8,14 +8,9 @@ import {
   User
 } from 'firebase/auth'
 
-const app = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJET_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-})
+import { firebaseAppConfig } from '@/utils/firebaseAppConfig'
+
+const app = initializeApp(firebaseAppConfig)
 
 const provider = new GoogleAuthProvider()
 
