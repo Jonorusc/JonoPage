9f63dcd90a3ca909e00dde36d44602fb49a76563
João Lucas <joaolucascp16@gmail.com>
2023-06-28 11:44:02 -0300

    Update index.ts

--- src/firebase/crud/index.ts
+++ src/firebase/crud/index.ts
@@ -12,15 +12,8 @@ import {
   getFirestore
 } from 'firebase/firestore'
 import { getStorage, ref, uploadString } from 'firebase/storage'
-
-const app = initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJET_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
-})
+import { firebaseAppConfig } from '@/utils/firebaseAppConfig'
+const app = initializeApp(firebaseAppConfig)
 
 const db = getFirestore(app)
 
