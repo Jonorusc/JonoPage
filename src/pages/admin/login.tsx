4e6b84edc4f19822c4e288dba7a04f8ecc0f47b1
João Lucas <joaolucascp16@gmail.com>
2023-06-29 14:01:34 -0300

    update(pages/admin/login/index.tsx): using Notify component

--- src/pages/admin/login.tsx
+++ src/pages/admin/login.tsx
@@ -1,17 +1,31 @@
 import LoginPage from '@/templates/Admin/Login'
 import { signInWithGoogle } from '@/firebase/auth'
 import { useRouter } from 'next/router'
+import Notify from '@/components/Notify'
+import { useState } from 'react'
+
 const Login = () => {
+  const [message, setMessage] = useState('' as string)
   const router = useRouter()
 
   const onSubmit = () =>
-    signInWithGoogle().then(() => {
-      router.push('/admin/')
-    })
+    signInWithGoogle()
+      .then(() => {
+        router.push('/admin/')
+      })
+      .catch((error) => {
+        setMessage(error.message)
+      })
 
   return (
     <main>
       <LoginPage title="Prove that you're Lucas (me)" onSubmit={onSubmit} />
+      <Notify
+        message={message}
+        type="error"
+        visible={!!message}
+        onClose={() => setMessage('')}
+      />
     </main>
   )
 }
