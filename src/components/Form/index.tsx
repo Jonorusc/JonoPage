dbf60528516ef70db2c1de1d3260da4274aeb5ac
João Lucas <joaolucascp16@gmail.com>
2023-07-01 16:31:43 -0300

    update(components/Form): improving code
    taking target element from the form event instead of useRef hook

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useState, useRef } from 'react'
+import React, { useState } from 'react'
 import { FormProps, ChildrenProps, InputValue } from '@/types/form'
 import { InputProps } from '@/types/input'
 import { FileProps } from '@/types/file'
@@ -16,7 +16,6 @@ const Form = ({
 }: FormProps) => {
   const onResetAvailable = !!onReset && typeof onReset === 'function'
   const onErrorAvailable = !!onError && typeof onError === 'function'
-  const formRef = useRef<HTMLFormElement>(null)
   const [formValues, setFormValues] = useState<InputValue>({})
 
   // handle the form invalid event
@@ -28,68 +27,67 @@ const Form = ({
 
   const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
-    if (formRef.current) {
-      const childType = ['text', 'textarea', 'password', 'email']
+    const childType = ['text', 'textarea', 'password', 'email']
 
-      const InputComponentsName = (children: ChildrenProps): string[] => {
-        const names: string[] = []
+    const InputComponentsName = (children: ChildrenProps): string[] => {
+      const names: string[] = []
 
-        React.Children.forEach(children, (child) => {
-          if (
-            (React.isValidElement<InputProps<any>>(child) &&
-              childType.includes(child.props.type)) ||
-            (React.isValidElement<FileProps<any>>(child) && child.props.name)
-          ) {
-            names.push(child.props.name)
-          } else if (
-            React.isValidElement(child) &&
-            React.Children.count(child.props.children) > 0
-          ) {
-            const updatedChildren = InputComponentsName(child.props.children)
-            names.push(...updatedChildren)
-          }
-        })
+      React.Children.forEach(children, (child) => {
+        if (
+          (React.isValidElement<InputProps<any>>(child) &&
+            childType.includes(child.props.type)) ||
+          (React.isValidElement<FileProps<any>>(child) && child.props.name)
+        ) {
+          names.push(child.props.name)
+        } else if (
+          React.isValidElement(child) &&
+          React.Children.count(child.props.children) > 0
+        ) {
+          const updatedChildren = InputComponentsName(child.props.children)
+          names.push(...updatedChildren)
+        }
+      })
 
-        return names
-      }
-
-      const inputNames = InputComponentsName(children)
-      const elements = formRef.current.elements as HTMLFormControlsCollection
-
-      const dataObject = Array.from(elements).reduce(
-        (acc: InputValue, element: any) => {
-          if (inputNames.includes(element.id)) {
-            if (element.type === 'file') acc[element.id] = element.files
-            else acc[element.id] = element.value
-          }
-          return acc
-        },
-        {}
-      )
-      setFormValues(dataObject)
-
-      // check if there is any error (required fields)
-      const errorObject = Array.from(elements).reduce(
-        (acc: InputValue, element: any) => {
-          if (
-            (element.required && !element.value) ||
-            (element.requerid && element.files.length === 0)
-          ) {
-            acc[element.id] = 'This field is required'
-          }
-          return acc
-        },
-        {}
-      )
-
-      if (Object.keys(errorObject).length > 0) {
-        handleFormError(errorObject)
-        return
-      }
-
-      handleFormError({})
-      onSubmit(event, dataObject)
+      return names
     }
+
+    const inputNames = InputComponentsName(children)
+    const formRef = event.currentTarget as HTMLFormElement
+    const elements = formRef.elements as HTMLFormControlsCollection
+
+    const dataObject = Array.from(elements).reduce(
+      (acc: InputValue, element: any) => {
+        if (inputNames.includes(element.id)) {
+          if (element.type === 'file') acc[element.id] = element.files
+          else acc[element.id] = element.value
+        }
+        return acc
+      },
+      {}
+    )
+    setFormValues(dataObject)
+
+    // check if there is any error (required fields)
+    const errorObject = Array.from(elements).reduce(
+      (acc: InputValue, element: any) => {
+        if (
+          (element.required && !element.value) ||
+          (element.requerid && element.files.length === 0)
+        ) {
+          acc[element.id] = 'This field is required'
+        }
+        return acc
+      },
+      {}
+    )
+
+    if (Object.keys(errorObject).length > 0) {
+      handleFormError(errorObject)
+      return
+    }
+
+    handleFormError({})
+    onSubmit(event, dataObject)
   }
 
   const handleFormReset = (event: React.FormEvent<HTMLFormElement>) => {
@@ -101,10 +99,11 @@ const Form = ({
     )
     setFormValues(emptyFormValues)
     // reset form
-    if (formRef.current) {
-      const elements = formRef.current.elements as HTMLFormControlsCollection
-      Array.from(elements).forEach((element: any) => (element.value = ''))
-    }
+    const formRef = event.currentTarget as HTMLFormElement
+    const elements = formRef.elements as HTMLFormControlsCollection
+    Array.from(elements).forEach((element: any) => (element.value = ''))
+    console.log(elements)
+    formRef.reset()
 
     if (onResetAvailable) {
       onReset(event, emptyFormValues)
@@ -112,12 +111,7 @@ const Form = ({
   }
 
   return (
-    <S.Form
-      ref={formRef}
-      onReset={handleFormReset}
-      onSubmit={handleFormSubmit}
-      noValidate
-    >
+    <S.Form onReset={handleFormReset} onSubmit={handleFormSubmit} noValidate>
       {children}
       <S.FormButtons>
         {onResetAvailable ? <Button type="reset" text={resetName} /> : null}
