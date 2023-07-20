f28e319e59cbd435b83c3c0877135f2515cc0650
João Lucas <joaolucascp16@gmail.com>
2023-07-19 21:28:52 -0300

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,10 @@
 import AdminTemplate from '@/templates/Admin'
+import { NextSeo } from 'next-seo'
 
 const Page404 = () => {
   return (
     <AdminTemplate link="/" linkLabel="Back to landing page" title="404">
+      <NextSeo title="🤔 What happend?" />
       <h1>Sorry but i didn't create this page :(</h1>
     </AdminTemplate>
   )
