8ef94255720d570f8ca2738eacb997b8f1f1844a
João Lucas <joaolucascp16@gmail.com>
2023-08-09 13:23:19 -0300

    fix(components/DownloadCv): typing stories

--- src/components/DownloadCv/stories.tsx
+++ src/components/DownloadCv/stories.tsx
@@ -3,12 +3,19 @@ import DownloadCv from '.'
 
 const meta: Meta<typeof DownloadCv> = {
   title: 'DownloadCv',
-  component: DownloadCv
+  component: DownloadCv,
+  args: {
+    resumes: {
+      english: 'https://www.google.com',
+      portuguese: 'https://www.google.com'
+    },
+    open: true
+  }
 }
 
 export default meta
 type Story = StoryObj<typeof DownloadCv>
 
 export const Primary: Story = {
-  render: () => <DownloadCv />
+  render: (args) => <DownloadCv {...args} />
 }
